ed9da4dad63247802405ed323ad83f08f59795be
Anderson Fagundes Martins <anderson.fagundes@gmail.com>
2020-08-14 01:56:31 -0300

    Adiciona a instancia de PrinterProtocol no segundo construtor de DApplication
       Quando o programa tentava acessar o objeto de PrinterProtocol o firmware quebrava pois o objeto
     ainda não havia sido criado, causando um "null pointer access".

--- PrinterMod/dapplication.cpp
+++ PrinterMod/dapplication.cpp
@@ -7,7 +7,7 @@ DApplication *DApplication::instance_ = nullptr;
 /**
  * @brief DApplication::DApplication
  */
-DApplication::DApplication() {m_ptr_Protocol = new PrinterProtocol;}
+DApplication::DApplication() { m_ptr_Protocol = new PrinterProtocol; }
 
 /**
  * @brief DApplication::DApplication
@@ -17,6 +17,7 @@ DApplication::DApplication() {m_ptr_Protocol = new PrinterProtocol;}
 DApplication::DApplication(int argc, char *argv[]) {
   (void)argc;
   (void)argv;
+  m_ptr_Protocol = new PrinterProtocol;
 }
 
 /**
@@ -47,11 +48,10 @@ DApplication *DApplication::getInstance(int argc, char *argv[]) {
  * @brief DApplication::exec
  */
 void DApplication::exec() {
-    LOG_SCOPE_FUNCTION(INFO);
-    LOG_F(INFO, "Printer Daemon Started!! Checking for printer Device...");
-
-    //m_ptr_Protocol->setSerial("I'm ALIVE !!!!!!");
-    //m_ptr_Protocol->prepare_json_object();
+  LOG_SCOPE_FUNCTION(INFO);
+  LOG_F(INFO, "Printer Daemon Started!! Checking for priner Device...");
+  this->m_ptr_Protocol->setSerial("I'm ALIVE !!!!!!");
+  this->m_ptr_Protocol->prepare_json_object();
 
   while (1) {
   }
--- PrinterMod/printerprotocol.cpp
+++ PrinterMod/printerprotocol.cpp
@@ -1,7 +1,7 @@
 #include "printerprotocol.h"
 
-#include <stdio.h>
 #include <json-c/json.h>
+#include <stdio.h>
 
 /**
  * @brief PrinterProtocol::PrinterProtocol
@@ -9,44 +9,42 @@
 PrinterProtocol::PrinterProtocol() {}
 
 void PrinterProtocol::create_json_object(struct json_object *jobj, const char *a, void *b, int variable_b_type) {
-    
-    switch(variable_b_type) {
-        case TYPE_NULL:
-            break;
- 
-        case TYPE_BOOLEAN:
-            break;
- 
-        case TYPE_DOUBLE:
-            break;
- 
-        case TYPE_INT:
-            json_object_object_add(jobj, a, json_object_new_int(*((int *)b)));
-            break;
- 
-        case TYPE_STRING:
-            json_object_object_add(jobj, a, json_object_new_string((const char *)b));
-            break;
- 
-        case TYPE_OBJECT:
-            break;
- 
-        case TYPE_ARRAY:
-            break;
-    }
+  switch (variable_b_type) {
+    case TYPE_NULL:
+      break;
+
+    case TYPE_BOOLEAN:
+      break;
+
+    case TYPE_DOUBLE:
+      break;
+
+    case TYPE_INT:
+      json_object_object_add(jobj, a, json_object_new_int(*((int *)b)));
+      break;
+
+    case TYPE_STRING:
+      json_object_object_add(jobj, a, json_object_new_string((const char *)b));
+      break;
+
+    case TYPE_OBJECT:
+      break;
+
+    case TYPE_ARRAY:
+      break;
+  }
 }
 
 void PrinterProtocol::prepare_json_object(void) {
-    
- struct json_object *jobj;
- jobj = json_object_new_object();
+  struct json_object *jobj;
+  jobj = json_object_new_object();
 
- create_json_object(jobj, "serial", (void *)serial().c_str(), TYPE_STRING);
+  create_json_object(jobj, "serial", (void *)serial().c_str(), TYPE_STRING);
 
- //printf("jobj from str:\n---\n%s\n---\n", json_object_to_json_string_ext(jobj, JSON_C_TO_STRING_SPACED | JSON_C_TO_STRING_PRETTY));
- cout << "jobj from str:" << endl;
- cout << "---" << endl;
- cout << json_object_to_json_string_ext(jobj, JSON_C_TO_STRING_SPACED | JSON_C_TO_STRING_PRETTY) << endl;
- cout << "---" << endl;
-
+  // printf("jobj from str:\n---\n%s\n---\n", json_object_to_json_string_ext(jobj, JSON_C_TO_STRING_SPACED |
+  //   JSON_C_TO_STRING_PRETTY));
+  cout << "jobj from str:" << endl;
+  cout << "---" << endl;
+  cout << json_object_to_json_string_ext(jobj, JSON_C_TO_STRING_SPACED | JSON_C_TO_STRING_PRETTY) << endl;
+  cout << "---" << endl;
 }
